2b0493e802e7996500379e3b4cd8a04eee97e8eb
ninyong3 <yongpoke06@gmail.com>
2025-07-31 01:11:37 +0900

    7/30

--- Assets/script/SystemManager.cs
+++ Assets/script/SystemManager.cs
@@ -1,10 +1,29 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 public class SystemManager : MonoBehaviour
 {
     [SerializeField] GameObject dialog;
     Coroutine waitClick;
     int check=0;
+    GameObject currentClickObject;
+    GameObject previousClickObject;
+    [SerializeField] GameObject Closeimage;
+    [SerializeField] GameObject Titlewarningimage;
+    [SerializeField] GameObject Skipwarningimage;
+    void Update()
+    {
+        if (previousClickObject != Closeimage)
+            check = 0;
+        previousClickObject = currentClickObject;
+        currentClickObject = EventSystem.current.currentSelectedGameObject;
+    }
+    void Start()
+    {
+        Titlewarningimage.SetActive(false);
+        Skipwarningimage.SetActive(false);
+    }
     public void CloseSystem()
     {
         if (check == 0)
@@ -23,5 +42,30 @@ public class SystemManager : MonoBehaviour
             yield return null;
         }
         dialog.SetActive(true);
+    }
+    public void ToTitleSystem()
+    {
+        Titlewarningimage.SetActive(true);
+    }
+    public void ToTitleClickYes()
+    {
+      SceneManager.LoadScene("title");
+    }
+    public void ToTitleClickNo()
+    {
+        Titlewarningimage.SetActive(false);
+    }
+    public void SkipDaySystem()
+    {
+        Skipwarningimage.SetActive(true);
+    }
+    public void SkipDayClickYes()
+    {
+        GameManager.instance.dayCount++;
+        Skipwarningimage.SetActive(false);
+    }
+    public void SkipDayClickNo()
+    {
+        Skipwarningimage.SetActive(false);
     }
 }
