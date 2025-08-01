e30d14951c752987feeb3763acba3267b4e396de
ninyong3 <yongpoke06@gmail.com>
2025-08-01 19:23:04 +0900

    8/1 - 2

--- Assets/script/MessengerManager.cs
+++ Assets/script/MessengerManager.cs
@@ -1,9 +1,13 @@
-using UnityEngine;
 using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 public class MessengerManager : MonoBehaviour
 {
     [SerializeField] GameObject messenger;
     Vector2 messengerPosition;
+    [SerializeField] RectTransform chatContentRect;
+    [SerializeField] TextMeshProUGUI characterName;
     void Start()
     {
         
@@ -40,5 +44,28 @@ public class MessengerManager : MonoBehaviour
             messenger.GetComponent<RectTransform>().anchoredPosition = messengerPosition;
             yield return new WaitForSeconds(0.01f);
         }
+    }
+    public void ChatToNext()
+    {
+        GameObject heroineChatPrefab = Resources.Load<GameObject>("Leftmessagecontrol");
+        GameObject heroineChatGo = GameObject.Instantiate(heroineChatPrefab, chatContentRect);
+        GameObject PlayerChatPrefab = Resources.Load<GameObject>("Rightmessagecontrol");
+        GameObject PlayerChatGo = GameObject.Instantiate(PlayerChatPrefab, chatContentRect);
+        Transform chatText = PlayerChatGo.transform.Find("Chatimage").Find("Chattext");
+        chatText.GetComponent<TextMeshProUGUI>().text = "테스트";
+        Transform characterProfileImage = heroineChatGo.transform.Find("CharacterProfile");
+      //  characterProfileImage.gameObject.SetActive(false);
+    }
+    public void SelectJinyeinMessage()
+    {
+        characterName.text = "진예인";
+    }
+    public void SelectFreyjaMessage()
+    {
+        characterName.text = "프레이야 레가토";
+    }
+    public void SelectRuMessage()
+    {
+        characterName.text = "루";
     }
 }
