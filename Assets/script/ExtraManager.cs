390f31dda3e31ae67886e263af0c9bb04927ab9b
ninyong3 <yongpoke06@gmail.com>
2025-08-16 21:53:53 +0900

    .

---
+++ Assets/script/ExtraManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ExtraManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void GoToNormalEnd()
+    {
+        
+    }
+
+}
